2d769f8529c7f0341a3b96a1d0e54682f4ad1d1d
BrooksPoltl <bpoltl1@gmail.com>
2019-04-22 11:43:39 -0500

    Add files via upload

---
+++ server/knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+localPbConnection = {
+  host: 'localhost',
+  database: 'games',
+  user: 'Elan',
+  password: 'pass'
+}
+
+const prodDbConnection = process.env.DATABASE_URL || localPbConnection
+
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/tracker.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: prodDbConnection,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }
+};
